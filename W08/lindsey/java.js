df06e06edadc658d08770e93a0afbc861fd15b4d
Lindsey Myers <candlmyers1012@gmail.com>
2021-11-04 09:46:03 -0600

    w08 lindsey commented and cleaned up javascript code

--- W08/lindsey/java.js
+++ W08/lindsey/java.js
@@ -1,4 +1,5 @@
 
+// Fetch url function
 async function getAPI(url) {
   try {
     const response = await fetch(url);
@@ -10,13 +11,14 @@ async function getAPI(url) {
   }
 }
 
+//Main driving function
 async function loadCharacters(url="https://swapi.dev/api/people/") {
   let myData = await getAPI(url);
   renderCharacters(myData.results);
-  //console.log(myData);
   const next = document.getElementById("next_button");
   const prev = document.getElementById("previous_button");
 
+  //Next Button
   if (myData.next != null) {  
     next.classList.remove("hidden")
     next.onclick = () => {
@@ -26,6 +28,7 @@ async function loadCharacters(url="https://swapi.dev/api/people/") {
     next.classList.add("hidden");
   }
 
+  //Previous Button
   if (myData.previous != null) {
     prev.classList.remove("hidden");
     prev.onclick = () => {
@@ -35,12 +38,13 @@ async function loadCharacters(url="https://swapi.dev/api/people/") {
     prev.classList.add("hidden");
   }
 
+  // Load page number links
   if (myData.count > 10) {
-    //console.log(myData);
     showPages(myData.count);
   }
 }
 
+//Display the character list
 function renderCharacters(characterList) {
   let list = document.getElementById("character_list");
   list.innerHTML = "";
@@ -58,11 +62,13 @@ function renderCharacters(characterList) {
   })
 }
 
+//Load character details from API
 async function getCharacterDetails(url) {
   let character = await getAPI(url);
   renderCharacterDetails(character);
 }
 
+//Display character details
 function renderCharacterDetails(character) {
   let detailOutput = document.getElementById(`${character.name}details`);
   if (detailOutput.innerHTML === "") {
@@ -77,11 +83,11 @@ function renderCharacterDetails(character) {
   
 }
 
+//Determine number of page links to display and display them
 function showPages(count) {
   const pagesOutput = document.getElementById("page_buttons");
   pagesOutput.innerHTML = "";
   let numPages = Math.ceil(count / 10);
-  //console.log(numPages);
 
   for (let i = 1; i <= numPages; i++) {
     let item = document.createElement("span");
@@ -89,6 +95,7 @@ function showPages(count) {
     if (i < numPages) item.innerHTML = `${i} - `;
     else item.innerHTML = `${i}`;
 
+    //Add event listener to page numbers
     item.addEventListener("click", function (event) {
       event.preventDefault();
       loadCharacters(`https://swapi.dev/api/people/?page=${i}`);
